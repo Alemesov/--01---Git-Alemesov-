af0e0ed071a47819cf923c5871d676b508115c8e
Алемесов Степан <alemesov@inbox.ru>
2018-06-27 01:55:49 +0400

    third commit

--- Python.py
+++ Python.py
@@ -1,3 +1,4 @@
-Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
+x=int(input())
+y=int(input())
+print("Ответ")
+print(x+y)
